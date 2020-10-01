7ae39572f23e6b19b9a45e6997a0c1871967a04f
khadepratiksha <35521842+khadepratiksha@users.noreply.github.com>
2020-10-01 17:55:08 +0530

    Add files via upload

---
+++ Assign2_Q9.java
@@ -0,0 +1,22 @@
+package practice2222;
+class Assign2_Q9
+{
+public static void main(String args[])
+{
+    int rows=6;
+for(int i=rows;i>=1;i--)
+{
+for(int j=1;j<=i;j++)
+{
+System.out.print("* ");
+}
+System.out.print("\n");
+}
+
+
+
+}
+
+
+
+}
