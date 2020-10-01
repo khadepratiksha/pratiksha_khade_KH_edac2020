fdb27fecfcc735042a518a84f5a94f59b3b6c547
khadepratiksha <35521842+khadepratiksha@users.noreply.github.com>
2020-10-01 17:48:57 +0530

    Add files via upload

---
+++ Assign2Q1.java
@@ -0,0 +1,19 @@
+package practice2222;
+class Assign2Q1{
+public static void main(String []args)
+{
+for(int i=1;i<=9;i++)
+{
+for (int j=9-i; j>=1; j--)
+{
+System.out.print(" "); 
+}
+for(int j=1;j<=i;j++)
+{
+System.out.print(" "+i);
+
+}
+System.out.print("\n");
+}
+}
+}
