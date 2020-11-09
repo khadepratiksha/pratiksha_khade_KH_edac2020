3b1f441b76568b9668cc35c0f830682ddb60077c
khadepratiksha <35521842+khadepratiksha@users.noreply.github.com>
2020-11-09 21:13:10 +0530

    Add files via upload

---
+++ Assign5Q8.java
@@ -0,0 +1,17 @@
+package StringAssignment;
+
+public class Assign5Q8 {
+	
+	
+	    
+	public static void main(String[] args)
+	{
+	     String string="Hello World";
+	     
+	    String reverse = new StringBuffer(string).reverse().toString();
+	      System.out.println("\nString before reverse: "+string);
+	      System.out.println("String after reverse: "+reverse);
+	    
+	
+	}
+}
